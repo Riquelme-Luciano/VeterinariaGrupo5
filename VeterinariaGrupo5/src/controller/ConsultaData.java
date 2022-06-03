1dee9d58e486b4116148d8d17607420f585f4927
Luciano Riquelme <87220311+Riquelme-Luciano@users.noreply.github.com>
2022-06-03 20:05:16 -0300

    Add files via upload

---
+++ VeterinariaGrupo5/src/controller/ConsultaData.java
@@ -0,0 +1,33 @@
+
+package controller;
+
+import model.Conexion;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConsultaData {
+    private Connection con=null;
+    
+    public ConsultaData(Conexion conexion) {
+        this.con = conexion.getConexion();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+}
---
+++ VeterinariaGrupo5/src/model/TipoMascota.java
@@ -0,0 +1,20 @@
+package model;
+
+public enum TipoMascota {
+    PERRO("Perro"),
+    GATO("Gato"),
+    LORO("Loro"),
+    HAMSTER("Hamster");
+    
+    private String especie;
+
+    private TipoMascota(String especie) {
+        this.especie = especie;
+    }
+
+    @Override
+    public String toString() {
+        return "Especie=" + especie + '}';
+    }
+       
+}
